d512a46453a5bc9c673ad7a990b5e749f7e12ee0
simonadeboradeleanu <99869037+simonadeboradeleanu@users.noreply.github.com>
2023-03-29 19:49:44 +0300

    login

---
+++ app/src/main/java/com/example/sportmatch/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.sportmatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameEditText = findViewById(R.id.activity_main_usernameEditText);
+        passwordEditText = findViewById(R.id.activity_main_passwordEditText);
+        loginButton = findViewById(R.id.activity_main_loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (usernameEditText.getText().length() > 0 && passwordEditText.getText().length() > 0) {
+                    String toastMessage = "Username: " + usernameEditText.getText().toString() + ", Password: " + passwordEditText.getText().toString();
+                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+                } else {
+                    String toastMessage = "Username or Password are not populated";
+                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
