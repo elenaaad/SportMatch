b267b1991ca85bc2d80d01eaca9192a6f3b537bb
Raluca Andreea Ionescu <ralucaandreeai@yahoo.com>
2023-04-01 21:36:36 +0300

    Bottom Navigation Added

---
+++ app/src/main/java/com/example/sportmatch/AdminEventsActivity.java
@@ -0,0 +1,50 @@
+package com.example.sportmatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AdminEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_events);
+
+        ////inceput meniu
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_admin_events);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.bottom_admin_events:
+                    return true;
+                case R.id.bottom_home:
+                    startActivity(new Intent(getApplicationContext(), BottomNavActivity.class));
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    finish();
+                    return true;
+                case R.id.bottom_create_event:
+                    startActivity(new Intent(getApplicationContext(), CreateNewEventActivity.class));
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    finish();
+                    return true;
+                case R.id.bottom_events_participates:
+                    startActivity(new Intent(getApplicationContext(), OnlyParticipatesEvents.class));
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    finish();
+                    return true;
+                case R.id.bottom_view_profile:
+                    startActivity(new Intent(getApplicationContext(), ViewProfileActivity.class));
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    finish();
+                    return true;
+            }
+            return false;
+        });
+        ////final meniu
+    }
+}
