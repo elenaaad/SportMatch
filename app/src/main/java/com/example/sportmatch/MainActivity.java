5bcee2e75ea72ac0f29af376ab81ba7575dc8837
bianca <alexiabianca2002@gmail.com>
2023-04-01 15:20:54 +0300

    aolo

--- app/src/main/java/com/example/sportmatch/MainActivity.java
+++ app/src/main/java/com/example/sportmatch/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-//sunt in branch?
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
